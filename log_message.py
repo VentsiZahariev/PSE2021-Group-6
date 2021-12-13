e6c31eba8907ebad4f3cdf5301960d044d02f0e8
Tim ter Steege <425283@student.saxion.nl>
2021-12-13 23:12:03 +0100

    added log_message.py file

---
+++ log_message.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+# logger.py
+# Last edit made: 10-12-2021
+# Version: 0.1
+# Subject: Logs error messages to a output file to catch crashes and failures
+# Author: Tim ter Steege
+# python version used: 3.9
+
+import logging
+
+
+def info_log(message):
+    try:
+        log_format = "%(levelname)s: [%(asctime)s] - %(message)s"
+        logging.basicConfig(filename="logfile.out",
+                            filemode="a",
+                            format=log_format,
+                            level=logging.INFO)
+        logger = logging.getLogger()
+        logger.info(message)
+
+    except AttributeError as e:
+        print("Error: ", e)
+
+
+def warning_log(message):
+    try:
+        log_format = "%(levelname)s: [%(asctime)s] - %(message)s"
+        logging.basicConfig(filename="logfile.out",
+                            filemode="a",
+                            format=log_format,
+                            level=logging.WARNING)
+        logger = logging.getLogger()
+        logger.warning(message)
+
+    except AttributeError as e:
+        print("Error: ", e)
+
+
+def error_log(message):
+    try:
+        log_format = "%(levelname)s: [%(asctime)s] - %(message)s"
+        logging.basicConfig(filename="logfile.out",
+                            filemode="a",
+                            format=log_format,
+                            level=logging.ERROR)
+        logger = logging.getLogger()
+        logger.error(message)
+
+    except AttributeError as e:
+        print("Error: ", e)
+
+
+def critical_log(message):
+    try:
+        log_format = "%(levelname)s: [%(asctime)s] - %(message)s"
+        logging.basicConfig(filename="logfile.out",
+                            filemode="a",
+                            format=log_format,
+                            level=logging.CRITICAL)
+        logger = logging.getLogger()
+        logger.critical(message)
+
+    except AttributeError as e:
+        print("Error: ", e)
