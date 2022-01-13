7e2649ae588ad56467ef1f44e596b96d357a298b
Tim ter Steege <71231148+Centuriooo@users.noreply.github.com>
2022-01-13 12:23:10 +0100

    last modification with some small bug fixes and addition with a second broker

--- data_parser.py
+++ data_parser.py
@@ -8,84 +8,173 @@
 # python version used: 3.9
 
 import json
+import base64
+import binascii
+import log_message
+
+# Global variable for the filename for logging messages
+log_filename = "mqtt_log.out"
+
+def parse_eui(json, device):
+    try:
+        gateway_eui = json['uplink_message']['rx_metadata'][0]['gateway_ids']['eui']
+
+        return gateway_eui
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Gateway eui is not in string, device_id: " + device))
+        return None
 
 
-class data_parser:
-    def parse_eui(self):
-        try:
-            gateway_eui = self['uplink_message']['rx_metadata'][0]['gateway_ids']['eui']
+def parse_gatewayid(json, device):
+    try:
+        gateway_id = json['uplink_message']['rx_metadata'][0]['gateway_ids']['gateway_id']
 
-            return gateway_eui
-        except KeyError:
+        return gateway_id
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Gateway id is not in string, device_id: " + device))
+        return None
+
+
+def parse_rssi(json, device):
+    try:
+        gateway_rssi = json['uplink_message']['rx_metadata'][0]['rssi']
+
+        return gateway_rssi
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Gateway rssi is not in string, device_id: " + device))
+        return None
+
+
+def parse_crssi(json, device):
+    try:
+        gateway_crssi = json['uplink_message']['rx_metadata'][0]['channel_rssi']
+
+        return gateway_crssi
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Gateway channel rssi is not in string, device_id: " + device))
+        return None
+
+
+def parse_payload(json, device):
+    try:
+        payload = json['uplink_message']['frm_payload']
+
+        return payload
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Payload field is not in string, device_id: " + device))
+        return None
+
+
+def parse_deviceid(json):
+    try:
+        device_id = json['end_device_ids']['device_id']
+        return device_id
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Device id is not in string, with json:"))
+        return None
+
+def parse_latitude(json_str, device):
+    try:
+        data = json.loads(json_str)
+        latitude = data['uplink_message']['rx_metadata'][0]['location']['latitude']
+        return latitude
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Device id is not in string, with json: " + device))
+        return None
+
+
+def parse_longitude(json_str, device):
+    try:
+        data = json.loads(json_str)
+        longitude = data['uplink_message']['rx_metadata'][0]['location']['longitude']
+        return longitude
+    except KeyError:
+        # log to file a field is not in string
+        log_message.warning_log(("Device id is not in string, with json: " + device))
+        return None
+
+
+def parse_metadata(json_str, device):
+    """
+    This function parse the json string received from the broker.
+    it will get the some metadata from the thigs network message thats stored in the json string
+
+    :return: returns the gateway_id, gateway_eui, gateway_rssi and gateway_channel-rssi as a tuple
+    """
+    data = json.loads(json_str)
+
+    eui = parse_eui(data, device)
+    gateway_id = parse_gatewayid(data, device)
+    rssi = parse_rssi(data, device)
+    crssi = parse_crssi(data, device)
+
+    return gateway_id, eui, rssi, crssi
+
+
+def parse_datavalues(json_str):
+    """
+    This function parse the json string received from the broker.
+    it will get the payload field within the json-string,
+    wich contains the bytes with the data for the light, pressure and temperature for the py and lht.
+    For the lht device, also the battery status.
+
+    In the second part it is also parsing the device id field to retrieve the device id.
+
+    :return: returns the payload field and the id as a tuple
+    """
+
+    data = json.loads(json_str)
+    device_id = parse_deviceid(data)
+    payload = parse_payload(data, device_id)
+
+    return payload, device_id
+
+
+def decode_payload(payload, device):
+    """
+    This function is used to decode the raw payload field for the pycom device.
+    This function is used to decode the raw payload field for the Dragino LHT65 device.
+
+    :param payload: This contains the raw payload retrieved from the json string
+    :return: it will return "pycom", pressure, light, temperature when payload contains 4 bytes
+            it will return "lht", pressure, light, temperature, batv when payload contains 11 bytes
+            it will return None when otherwise or a binascii.Error (when the payload can not be decoded) happens
+    """
+    try:
+        raw = base64.b64decode(payload)
+
+        if len(raw) == 4:
+            pressure = raw[0] / 2 + 950  # air pressure in mbar
+            light = raw[1]  # ambient light in lux???
+            temperature = ((raw[2] - 20) * 10 + raw[3]) / 10  # temperature in degree Celcius
+
+            return "pycom", pressure, light, temperature
+
+        elif len(raw) == 11:
+            pressure = (raw[4] << 8 | raw[5]) / 10  # Humidity pressure in %
+
+            light = (raw[7] << 8 | raw[8])  # Illumination in lux
+
+            temperature = (raw[2] << 8 | raw[3])  # temperature in degree Celcius
+            if raw[2] & 0x80:
+                temperature = temperature - (1 << 16)
+            temperature = temperature/100
+
+            batv = ((raw[0] << 8 | raw[1]) & 0x3FFF) / 1000  # battery voltage in volt
+
+            return "lht", pressure, light, temperature, batv
+
+        else:
+            log_message.warning_log(("Payload field does not contain the correct byte representation, device_id: " + device))
             return None
 
-    def parse_gatewayid(self):
-        try:
-            gateway_id = self['uplink_message']['rx_metadata'][0]['gateway_ids']['gateway_id']
-
-            return gateway_id
-        except KeyError:
-            return None
-
-    def parse_rssi(self):
-        try:
-            gateway_rssi = self['uplink_message']['rx_metadata'][0]['rssi']
-
-            return gateway_rssi
-        except KeyError:
-            return None
-
-    def parse_crssi(self):
-        try:
-            gateway_crssi = self['uplink_message']['rx_metadata'][0]['channel_rssi']
-
-            return gateway_crssi
-        except KeyError:
-            return None
-
-    def parse_payload(self):
-        try:
-            payload = self['uplink_message']['frm_payload']
-
-            return payload
-        except KeyError:
-            return None
-
-    def parse_deviceid(self):
-        try:
-            device_id = self['end_device_ids']['device_id']
-            return device_id
-        except KeyError:
-            return None
-
-    def parse_metadata(self):
-        """
-        This function parse the json string received from the broker.
-        it will get the some metadata from the thigs network message thats stored in the json string
-
-        :return: returns the gateway_id, gateway_eui, gateway_rssi and gateway_channel-rssi as a tuple
-        """
-        data = json.loads(self)
-        eui = data_parser.parse_eui(data)
-        gateway_id = data_parser.parse_gatewayid(data)
-        rssi = data_parser.parse_rssi(data)
-        crssi = data_parser.parse_crssi(data)
-
-        return gateway_id, eui, rssi, crssi
-
-    def parse_datavalues(self):
-        """
-        This function parse the json string received from the broker.
-        it will get the payload field within the json-string,
-        wich contains the bytes with the data for the light, pressure and temperature for the py and lht.
-        For the lht device, also the battery status.
-
-        In the second part it is also parsing the device id field to retrieve the device id.
-
-        :return: returns the payload field and the id as a tuple
-        """
-
-        data = json.loads(self)
-        payload = data_parser.parse_payload(data)
-        device_id = data_parser.parse_deviceid(data)
-        return payload, device_id
+    except binascii.Error:
+        log_message.warning_log(("Payload field does not contain the correct byte representation, device_id: " + device))
+        return None
--- mqtt_connection.py
+++ mqtt_connection.py
@@ -7,46 +7,59 @@
 # Author: Tim ter Steege
 # python version used: 3.9
 
-import binascii
-import sys
-import mariadb as mariadb
+
 import paho.mqtt.client as mqtt
 import time
-import base64
 from datetime import datetime
+import sys
+import mariadb as mariadb
 
 import data_parser as parser
-import logger
+import log_message
 
-# global variable to hold temporarily 1 message from the broker, this will be the json line
+# global variable to hold the json string received from
 json_data = None
 # global variable flag for the status of a newly received message
 message_flag = False
 # global variable flag for connection status
-connected_flag = False
+connected_flag1 = False
+connected_flag2 = False
+
+# Global variables with credentials to login to mqtt-broker
+mqtt_host1 = "eu1.cloud.thethings.network"
+mqtt_port1 = 8883
+mqtt_username1 = "project-software-engineering@ttn"
+mqtt_password1 = "NNSXS.DTT4HTNBXEQDZ4QYU6SG73Q2OXCERCZ6574RVXI.CQE6IG6FYNJOO2MOFMXZVWZE4GXTCC2YXNQNFDLQL4APZMWU6ZGA"
+
+# Global variables with credentials to login to mqtt-broker
+mqtt_host2 = "eu1.cloud.thethings.network"
+mqtt_port2 = 8883
+mqtt_username2 = "pse@ttn"
+mqtt_password2 = "NNSXS.3EQWSDBMUM4RSDHGXZW7TPKJNSH37G4TFFJRKXY.WUVUZDBBJUJQULSMWBOFZ6KILXYVJFC6JALF432GX4DRW3FQEJHA"
+
+# Global variables with credentials to login to database
+db_host = "127.0.0.1"
+db_port = 3306
+db_username = "PSEgroup6"
+db_password = "battlefield4"
+db_database = "weather_log"
+
+# Global variable for the filename for logging messages
+log_filename = "mqtt_log.out"
+
 
 try:
-    host_db = "127.0.0.1"
-    user_db = "PSEgroup6"
-    password_db = "battlefield4"
-    port_db = 3306
-    db = "weather_log"
-    mydb = mariadb.connect(user=user_db, password=password_db, host=host_db, port=port_db, database=db)
+    mydb = mariadb.connect(user=db_username, password=db_password, host=db_host, port=db_port, database=db_database)
 except mariadb.Error as err:
-    logger.log.error_log("cant connect to database")
+    # logging critical error to output file to let the user now the connection to the database cant be established
+    log_message.critical_log(("Can not connect to database: " + db_database))
+    log_message.critical_log("message: " + err)
+    log_message.info_log("Exit system with reason: No database connection. ")
     sys.exit(1)
 else:
-    logger.log.debug_log("connected to database")
-    myCursor=mydb.cursor()
-
-# Global variables with credentials for login to mqtt-broker
-host = "eu1.cloud.thethings.network"  # Broker address/host
-# port = 1883  # Normal broker port
-port = 8883  # Encrypted broker port
-# Broker username
-username = "project-software-engineering@ttn"
-# Broker password
-password = "NNSXS.DTT4HTNBXEQDZ4QYU6SG73Q2OXCERCZ6574RVXI.CQE6IG6FYNJOO2MOFMXZVWZE4GXTCC2YXNQNFDLQL4APZMWU6ZGA"
+    # logging info to output file to let the user now the connection to the database has been made
+    log_message.info_log(("connected to database: " + db_database))
+    myCursor = mydb.cursor()
 
 
 def on_message(client, userdata, message):
@@ -65,7 +78,7 @@ def on_message(client, userdata, message):
     message_flag = True
 
 
-def on_connect(client, userdata, flags, rc):
+def on_connect1(client, userdata, flags, rc):
     """
     Function that will check if the program is connected to the things network.
     it will set the connected flag to True if it's connected and False otherwise.
@@ -75,80 +88,92 @@ def on_connect(client, userdata, flags, rc):
     :param flags:
     :param rc: the connection code thats used to check if its conected to the things network
     """
-    global connected_flag
+
+    global connected_flag1
     if rc == 0:
-        logger.log.debug_log("Connected with result code")
-        connected_flag = True
+        # logging info to output file to let the user now the connection to the MQTT broker has been made
+        log_message.info_log(message=("Connected to the MQTT broker: " + mqtt_host1))
+        connected_flag1 = True
     else:
-        logger.log.debug_log("Not connected with result code")
-        connected_flag = False
+        # logging critical error to output file to let the user now the connection to the MQTT broker cant be established
+        log_message.info_log(message=("A missing connection to the MQTT broker: " + mqtt_host1))
+        connected_flag1 = False
 
 
-def mqtt_connect():
+def on_connect2(client, userdata, flags, rc):
+    """
+    Function that will check if the program is connected to the things network.
+    it will set the connected flag to True if it's connected and False otherwise.
+
+    :param client: The client to the things network/ connection
+    :param userdata: ??
+    :param flags:
+    :param rc: the connection code thats used to check if its conected to the things network
+    """
+
+    global connected_flag2
+    if rc == 0:
+        # logging info to output file to let the user now the connection to the MQTT broker has been made
+        log_message.info_log(message=("Connected to the MQTT broker: " + mqtt_host1))
+        connected_flag2 = True
+    else:
+        # logging critical error to output file to let the user now the connection to the MQTT broker cant be established
+        log_message.info_log(message=("A missing connection to the MQTT broker: " + mqtt_host1))
+        connected_flag2 = False
+
+
+def mqtt_connect1():
     """
     A function to connect to the things network.
     With the subscribe call, a message will be retrieved and will do a callback to the on_message function
 
     :return: returns the client its connected to
     """
+
     client = mqtt.Client("Python")  # create new instance
-    client.username_pw_set(username=username,
-                           password=password)  # set username and password
-    if port == 8883:
+    client.username_pw_set(username=mqtt_username1,
+                           password=mqtt_password1)  # set username and password
+    if mqtt_port1 == 8883:
         client.tls_set()
         client.tls_insecure_set(False)
-    client.on_connect = on_connect  # attach function to callback
+    client.on_connect = on_connect1  # attach function to callback
     client.on_message = on_message  # attach function to callback
     client.loop()
-    client.connect(host=host, port=port, keepalive=60)  # connect to broker
+    client.connect(host=mqtt_host1, port=mqtt_port1, keepalive=60)  # connect to broker
 
     client.loop_start()  # start the loop
-    while not connected_flag:  # Wait for connection
+    while not connected_flag1:  # Wait for connection
         time.sleep(0.1)
 
     client.subscribe("#")
     return client
 
 
-def decode_payload(payload):
+def mqtt_connect2():
     """
-    This function is used to decode the raw payload field for the pycom device.
-    This function is used to decode the raw payload field for the Dragino LHT65 device.
+    A function to connect to the things network.
+    With the subscribe call, a message will be retrieved and will do a callback to the on_message function
 
-    :param payload: This contains the raw payload retrieved from the json string
-    :return: it will return "pycom", pressure, light, temperature when payload contains 4 bytes
-            it will return "lht", pressure, light, temperature, batv when payload contains 11 bytes
-            it will return None when otherwise or a binascii.Error (when the payload can not be decoded) happens
+    :return: returns the client its connected to
     """
-    try:
-        raw = base64.b64decode(payload)
 
-        if len(raw) == 4:
-            pressure = raw[0] / 2 + 950  # air pressure in mbar
-            light = raw[1]  # ambient light in lux???
-            temperature = ((raw[2] - 20) * 10 + raw[3]) / 10  # temperature in degree Celcius
+    client = mqtt.Client("Python")  # create new instance
+    client.username_pw_set(username=mqtt_username2,
+                           password=mqtt_password2)  # set username and password
+    if mqtt_port1 == 8883:
+        client.tls_set()
+        client.tls_insecure_set(False)
+    client.on_connect = on_connect2  # attach function to callback
+    client.on_message = on_message  # attach function to callback
+    client.loop()
+    client.connect(host=mqtt_host2, port=mqtt_port2, keepalive=60)  # connect to broker
 
-            return "pycom", pressure, light, temperature
+    client.loop_start()  # start the loop
+    while not connected_flag2:  # Wait for connection
+        time.sleep(0.1)
 
-        elif len(raw) == 11:
-            pressure = (raw[4] << 8 | raw[5]) / 10  # Humidity pressure in %
-
-            light = (raw[7] << 8 | raw[8])  # Illumination in lux
-
-            temperature = (raw[2] << 8 | raw[3])  # temperature in degree Celcius
-            if raw[2] & 0x80:
-                temperature = temperature - (1 << 16)
-            temperature = temperature/100
-
-            batv = ((raw[0] << 8 | raw[1]) & 0x3FFF) / 1000  # battery voltage in volt
-
-            return "lht", pressure, light, temperature, batv
-
-        else:
-            return None
-
-    except binascii.Error:
-        return None
+    client.subscribe("#")
+    return client
 
 
 def run():
@@ -157,46 +182,59 @@ def run():
 
     it will exit the program when a keyboard interruptions happens.
     """
+
     global message_flag
-    client1 = mqtt_connect()
+    client1 = mqtt_connect1()
+    client2 = mqtt_connect2()
     try:
         while True:
             time.sleep(1)
             if message_flag:
-                payload, device_id = parser.data_parser.parse_datavalues(json_data)
-                data_values = decode_payload(payload)  # [0] = device name, [1] = pressure, [2] = light, [3] = temperature, ([4] = batv)
+                payload, device_id = parser.parse_datavalues(json_data)
+
                 # check if decoded values are valid
-                if data_values is not None:
-                    split_device_id = device_id.split("-")
-                    date_time = datetime.now()
-                    metadata = parser.data_parser.parse_metadata(json_data)  # [0] = gateway id, [1] = gateway eui, [2] = rssi, [3] = crssi
-                    try:
-                        if split_device_id[0]=="py":
-                            statement = "INSERT INTO pysense (log_time,location,temperature,pressure,light) VALUES (?,?,?,?,?)"
-                            values = (date_time, split_device_id[1], data_values[3], data_values[1], data_values[2])
-                            myCursor.execute(statement, values)
-                            statement2 = "INSERT INTO py_metadata (py_log_id,gateway_id,eui,r_signalstrength) VALUES (?,?,?,?)"
-                            values2 = ( myCursor.lastrowid, metadata[0], metadata[1], metadata[2])
-                            myCursor.execute(statement2, values2)
-                        elif split_device_id[0] == "lht":
-                            statement = "INSERT INTO dragino(log_time,location,temperature,humidity,light) VALUES (?,?,?,?,?)"
-                            values = (date_time,  split_device_id[1], data_values[3], data_values[1], data_values[2])
-                            myCursor.execute(statement, values)
-                            statement2 = "INSERT INTO dragino_metadata(dragino_id,gateway_id,eui,r_signalstrength) VALUES (?,?,?,?)"
-                            values2 = (myCursor.lastrowid, metadata[0], metadata[1], metadata[2])
-                            myCursor.execute(statement2, values2)
-                    except mariadb.Error as e:
-                        logger.log.error_log("not connected to database")  # logging to file
-                    mydb.commit()
+                if payload != None:
+                    data_values = parser.decode_payload(payload,device_id)  # [0] = device type, [1] = pressure, [2] = light, [3] = temperature, ([4] = batv)
+                    if data_values != None:
+                        split_device_id = device_id.split("-")
+                        date_time = datetime.now()
+                        metadata = parser.parse_metadata(json_data, device_id)  # [0] = gateway id, [1] = gateway eui, [2] = rssi, [3] = crssi
+                        try:
+                            if split_device_id[0] == "py":
+                                #data_values, time, metadata
+                                statement = "INSERT INTO pysense (device_id, log_time,location,temperature,pressure,light) VALUES (?,?,?,?,?,?)"
+                                values = (device_id, date_time, split_device_id[1], data_values[3], data_values[1], data_values[2])
+                                myCursor.execute(statement, values)
+                                statement2 = "INSERT INTO py_metadata (py_log_id,gateway_id,eui,r_signalstrength) VALUES (?,?,?,?)"
+                                values2 = ( myCursor.lastrowid, metadata[0], metadata[1], metadata[2])
+                                myCursor.execute(statement2, values2)
+                            elif split_device_id[0] == "lht":
+                                statement = "INSERT INTO dragino(device_id, log_time,location,temperature,humidity,light) VALUES (?,?,?,?,?,?)"
+                                values = (device_id, date_time,  split_device_id[1], data_values[3], data_values[1], data_values[2])
+                                myCursor.execute(statement, values)
+                                statement2 = "INSERT INTO dragino_metadata(dragino_id,gateway_id,eui,r_signalstrength) VALUES (?,?,?,?)"
+                                values2 = (myCursor.lastrowid, metadata[0], metadata[1], metadata[2])
+                                myCursor.execute(statement2, values2)
+                        except mariadb.Error as e:
+                            # logging a critical error to output file to let the user know a database connection has been lost
+                            log_message.warning_log(("A missing connection to the database: " + db_database))
+                        mydb.commit()
                 else:
-                    logger.log.debug_log("Device is not supported.") #logging to file
+                    # logging a warning reason to output file to let the user know a unvalid message has been transported over the things network
+                    log_message.warning_log(("Payload field is missing content, device: " + device_id))
 
                 message_flag = False
     except KeyboardInterrupt:
-        logger.log.debug_log("Exiting") #logging to file
+        # logging exit info to output file to let the user now when system has been shutdown and all the connections are closed
+        log_message.info_log("Exit system with reason: Keyboard Interruption.")
         client1.disconnect()
         client1.loop_stop()
+        client2.disconnect()
+        client2.loop_stop()
+        log_message.info_log("MQTT connection closed...")
         mydb.close()
+        log_message.info_log("Database connection closed...")
+        log_message.info_log("System closed.")
 
 
 if __name__ == '__main__':
